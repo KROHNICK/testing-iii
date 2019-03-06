8c5b8c262480477fe8de2127b199989c479e4fef
KROHNICK <business.kanekperez@gmail.com>
2019-03-06 13:09:13 -0800

    controls test complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
 // Test away!
+import React from "react";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  getByTestId,
+  getByText
+} from "react-testing-library";
+import Controls from "./Controls";
+import "jest-dom/extend-expect";
+
+describe("<Controls />", () => {
+  it("checks if buttons change state", () => {
+    const component = render(<Controls />);
+
+    component.getByTestId(/lockbutton/i);
+    component.getByTestId(/closebutton/i);
+  });
+
+  it("button displays close gate, lock gate status if open and unlocked", () => {
+    const component = render(<Controls />);
+
+    component.getByText(/close gate/i);
+    component.getByText(/lock gate/i);
+  });
+});
